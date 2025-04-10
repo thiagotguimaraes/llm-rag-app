0e39cde1ffa9aa6097c0bb4aae87c9b929822e4b
Thiago Guimaraes <thiagotguimaraes@gmail.com>
2025-04-10 14:49:38 +0400

    small fix

--- services/user-service/app/models/user.py
+++ services/user-service/app/models/user.py
@@ -3,7 +3,7 @@ from uuid import uuid4
 fake_users_db = {}
 
 class User:
-    def __init__(self, email: str, hashed_password: st, role: str = "user"):
+    def __init__(self, email: str, hashed_password: str, role: str = "user"):
         self.id = str(uuid4())
         self.email = email
         self.hashed_password = hashed_password
