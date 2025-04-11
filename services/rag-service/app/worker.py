d244accd68140e0a68a1bfc73749b6a1dfd36202
Thiago Guimaraes <thiagotguimaraes@gmail.com>
2025-04-11 18:48:11 +0400

    extracting all ports to the respectives .env files

--- services/rag-service/app/worker.py
+++ services/rag-service/app/worker.py
@@ -1,9 +1,14 @@
 from celery import Celery
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+REDIS_PORT = os.getenv("REDIS_PORT")
 
 celery_app = Celery(
     "rag_service_tasks",
-    broker="redis://redis:6379/0",  # for Docker
-    backend="redis://redis:6379/0",
+    broker=f"redis://redis:{REDIS_PORT}/0",  # for Docker
+    backend=f"redis://redis:{REDIS_PORT}/0",
 )
 
 celery_app.autodiscover_tasks(["app.tasks.add"])
