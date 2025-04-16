f36518d846e35bedb519756973f18e68c0b4d804
Thiago Guimaraes <thiagotguimaraes@gmail.com>
2025-04-16 11:21:04 +0400

    refactoring search endpoint

--- services/rag-service/app/api/v1/routes/search.py
+++ services/rag-service/app/api/v1/routes/search.py
@@ -1,22 +1,15 @@
-from fastapi import Query
-from app.qdrant_client import qdrant
+from fastapi import Query, Depends, APIRouter
+from app.api.auth import get_current_user_id
 from app.services.embedding import embedding_service
-from fastapi import APIRouter
+from app.qdrant_client import search_documents
+import logging
+
+logger = logging.getLogger(__name__)
 
 router = APIRouter()
 
 @router.get("/")
-async def search_documents(query: str = Query(...)):
-    vector = embedding_service.embed([query])
-    
-    result = qdrant.search(
-        collection_name="test-collection",
-        query_vector=vector,
-        limit=5,
-        with_payload=True,
-    )
-    print('result', result)
-    return [
-        {"text": hit.payload["text"], "score": hit.score}
-        for hit in result
-    ]
+def search(query: str = Query(...), topk: str = Query(...), user_id: str = Depends(get_current_user_id)):
+    vectors = embedding_service.embed(query)
+    search_result = search_documents(vectors, user_id=user_id, top_k=int(topk))
+    return search_result
