81d47428a8c2f2cd3585684f666cd83755193498
Thiago Guimaraes <thiagotguimaraes@gmail.com>
2025-04-18 12:48:46 +0400

    adding default jtw algorith veriable

--- services/user-service/app/core/security.py
+++ services/user-service/app/core/security.py
@@ -14,7 +14,7 @@ from sqlalchemy.future import select
 load_dotenv()
 
 JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
-JWT_ALGORITHM = os.getenv("JWT_ALGORITHM")
+JWT_ALGORITHM = os.getenv("JWT_ALGORITHM", "HS256")
 JWT_ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("JWT_ACCESS_TOKEN_EXPIRE_MINUTES", 15))
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
